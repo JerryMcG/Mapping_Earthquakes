167be2bc94f13b01fa6219f6d20d28d4d39f2a8e
JerryMcG <76828978+JerryMcG@users.noreply.github.com>
2021-09-30 19:50:25 -0400

    logic.js updates

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -23,29 +23,16 @@ let map = L.map('mapid', {
   center: [43.7,-79.3],
   zoom: 11,
   layers: [satelliteStreets]
-})
+});
 //pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 //getting airport geoJSON url
-let torontoHoods = "https://raw.githubusercontent.com/JerryMcG/Mapping_Earthquakes/main/torontoNeighbourhoods.json";
+let torontoHoods = "https://raw.githubusercontent.com/JerryMcG/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-//create a style for the lines
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data){
   console.log(data);
   //creating a GeoJson layer with the retrieved data
   L.geoJSON(data).addTo(map);
-    //, {
-    //style: myStyle,
-    //onEachFeature: function(feature, layer) {
-    //  layer.bindPopup("<h2>Airline: " + feature.properties.airline + "<h2> <hr> <h2>Destination: " 
-    //  + feature.properties.dst + "<h2>")
-    //}
-  //})
-  //.addTo(map);
 }); 
